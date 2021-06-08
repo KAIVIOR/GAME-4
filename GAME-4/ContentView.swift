74f36ba635857f6fd9ee64fa79703f6e83f87dec
KAIVIOR <wupeter99989@gmail.com>
2021-06-08 21:12:48 +0800

    Initial Commit

---
+++ GAME-4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GAME-4
+//
+//  Created by 吳庭愷 on 2021/6/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GAME-4/GAME_4App.swift
@@ -0,0 +1,17 @@
+//
+//  GAME_4App.swift
+//  GAME-4
+//
+//  Created by 吳庭愷 on 2021/6/8.
+//
+
+import SwiftUI
+
+@main
+struct GAME_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
